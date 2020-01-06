6884a5d794ea2037d3f875a1e930bff98d84348d
junya-0418 <jharada0418@gmail.com>
2020-01-06 22:53:44 +0900

    migration修正

--- database/migrations/2019_12_28_133806_create_checkins_table.php
+++ database/migrations/2019_12_28_133806_create_checkins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCheckinTable extends Migration
+class CreateCheckinsTable extends Migration
 {
     /**
      * Run the migrations.
